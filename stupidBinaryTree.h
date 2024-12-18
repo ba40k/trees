908a7870bb36e4dc992aa92429e035971f4f95d0
Radamir <dermeister20032132134@gmail.com>
2024-12-18 12:21:00 +0300

    Создал класс обычного бинарного дерева

---
+++ stupidBinaryTree.h
@@ -0,0 +1,15 @@
+//
+// Created by radamir on 18.12.24.
+//
+
+#ifndef STUPIDBINARYTREE_H
+#define STUPIDBINARYTREE_H
+#include "basicTree.h"
+template<typename T>
+class BinaryTree : public basicTree<T> {
+private:
+
+public:
+
+};
+#endif //STUPIDBINARYTREE_H
