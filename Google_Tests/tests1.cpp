46f3563c27144453cbe7e3d5899fb1562c16353a
Radamir <dermeister20032132134@gmail.com>
2024-12-21 00:34:05 +0300

    Подключил логи

--- Google_Tests/tests1.cpp
+++ Google_Tests/tests1.cpp
@@ -1,3 +1,4 @@
 //
 // Created by radamir on 21.12.24.
 //
+#include "gtest/gtest.h"
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 // Created by radamir on 18.12.24.
 //
 #include <iostream>
+#include "spdlog/include/spdlog/spdlog.h"
 int main() {
     std::cout << "Hello World!\n";
 }
